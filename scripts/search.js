1424a1136aee7530bcf68ed59802589f71648102
Sreekumar T <sreekumar.y2k@gmail.com>
2024-04-20 20:10:04 +0530

    Added search functionality client side, refactored style sheets,added showdown dist file

--- scripts/search.js
+++ scripts/search.js
@@ -1,21 +1,38 @@
+const mockData = [
+    { title: 'Caching Integers', url: 'java/j1' },
+    { title: 'Curious Case of Widening', url: 'java/j2' },
+    { title: 'String Allocations', url: 'java/j3' },
+    { title: 'String Allocations', url: 'java/j3' },
+    { title: 'String Allocations', url: 'java/j3' },
+    { title: 'String Allocations', url: 'java/j3' }
+];
+
+const BASE_URL = "https://glegshot.github.io/programmingdiaries/post?note=";
+
 $(document).ready(function() {
         const searchInput = $('#searchInput');
-        const delay = 2000; // Adjust delay as needed (in milliseconds)
+        const searchResults = document.getElementById('searchResults');
+        const delay = 2000; // delay in milliseconds
 
         // Debounced function for fetching data
-        const debouncedFetchData = debounce(function(query) {
-            fetchData(query);
-        }, delay);
+        //const debouncedFetchData = debounce(function(query) {
+        //    fetchData(query);
+        //}, delay);
 
         // Event listener for input change
         searchInput.on('input', function() {
-            const query = $(this).val();
-            if(query)
-                debouncedFetchData(query);
+            const query = $(this).val().trim();
+            if(query && query !== '') {
+                const results = fetchData(query); //debouncedFetchData(query);
+                displaySearchResults(results);
+            } else {
+                hideSearchResults();
+            }
         });
-    });
+});
 
-function debounce(func, delay) {
+// Function wrapper to debounce the input event 
+function debounce(func, delay, callback) {
         let timerId;
         return function() {
             clearTimeout(timerId);
@@ -23,8 +40,11 @@ function debounce(func, delay) {
         };
     }
 
-    // Function to perform AJAX request
-    function fetchData(query) {
+// Function to perform AJAX request to server
+function fetchData(query) {
+        console.log("invoking API for ", query);
+        console.log("returning mock data");
+        return mockData;
         // Perform AJAX request here
         /*$.ajax({
             url: 'your_api_endpoint',
@@ -39,5 +59,50 @@ function debounce(func, delay) {
                 console.error('Error:', error);
             }
         });*/
-        console.log("invoking API for ", query);
+}
+
+function displaySearchResults(results) {
+    searchResults.innerHTML = '';
+    results.forEach(result => {
+        const resultElement = document.createElement('div');
+        resultElement.textContent = result.title;
+        resultElement.classList.add('searchResultItem');
+        resultElement.addEventListener('click', function() {
+            // Handle click event
+            console.log('Clicked:', result.title);
+            onItemClick(result);
+            hideSearchResults(); // Close search results on click
+        });
+        searchResults.appendChild(resultElement);
+    });
+    showSearchResults(); // Show search results
+}
+
+function onItemClick(result) {
+    // Open URL in the same tab
+    url = BASE_URL + result.url
+    window.location.href = url;
+}
+
+
+// Function to show search results
+function showSearchResults() {
+    searchResults.style.display = 'block';
+    // Add click event listener to close search results when clicking outside of it
+    document.body.addEventListener('click', clickOutsideHandler);
+}
+
+function hideSearchResults() {
+    searchResults.style.display = 'none';
+    // Remove click event listener to avoid unnecessary checks
+    document.body.removeEventListener('click', clickOutsideHandler);
+}   
+
+// Click event handler for document body
+function clickOutsideHandler(event) {
+    if (!searchResults.contains(event.target) && event.target !== searchInput) {
+        // Clicked outside search results and search input, so hide search results
+        hideSearchResults();
     }
+}
+
