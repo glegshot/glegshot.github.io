bfc50d3ce1a788666c610b4504d58e1f4a22f806
Sreekumar T <sreekumar.y2k@gmail.com>
2024-04-20 13:44:34 +0530

    ajax call fixes

--- scripts/loader-1.0.js
+++ scripts/loader-1.0.js
@@ -12,42 +12,42 @@ $(document).ready(function() {
 				method: 'GET',
 				headers: {
 					'Accept': 'application/vnd.github.raw+json'
-			}
-			},
-			success: function(data) {
-			fileContent = data;
-			var frontMatterRegex = /^---([\s\S]*?)---/;
+				},
+				success: function(data) {
+					fileContent = data;
+					var frontMatterRegex = /^---([\s\S]*?)---/;
 
-			var frontmatter = fileContent.match(frontMatterRegex);
-			var markdownWithoutFrontMatter = fileContent;
-			
-			if(frontmatter) {
-				var metadata = frontmatter[1];
-				var splitMetaData = metadata.split(/\r\n/);
-				var title = splitMetaData.find(function(splitData) {
-					return splitData.includes("title");
-				});
+					var frontmatter = fileContent.match(frontMatterRegex);
+					var markdownWithoutFrontMatter = fileContent;
+					
+					if(frontmatter) {
+						var metadata = frontmatter[1];
+						var splitMetaData = metadata.split(/\r\n/);
+						var title = splitMetaData.find(function(splitData) {
+							return splitData.includes("title");
+						});
+						
+						title = title.split(': ');
+						
+						var keywords = splitMetaData.find(function(splitData) {
+							return splitData.includes("keywords");
+						});
+						
+						keywords = keywords.split(': ');
+						
+						$('meta[name="keywords"]').attr(keywords[1]);
+						$('title').text(title[1]);
+						
+						markdownWithoutFrontMatter = fileContent.replace(frontMatterRegex, ''); // Remove front matter
+					}
 				
-				title = title.split(': ');
-				
-				var keywords = splitMetaData.find(function(splitData) {
-					return splitData.includes("keywords");
-				});
-				
-				keywords = keywords.split(': ');
-				
-				$('meta[name="keywords"]').attr(keywords[1]);
-				$('title').text(title[1]);
-				
-				markdownWithoutFrontMatter = fileContent.replace(frontMatterRegex, ''); // Remove front matter
-			}
-		
-			var converter = new showdown.Converter();
-			var htmlContent = converter.makeHtml(markdownWithoutFrontMatter);
+					var converter = new showdown.Converter();
+					var htmlContent = converter.makeHtml(markdownWithoutFrontMatter);
 
-			htmlContent = converter.makeHtml(markdownWithoutFrontMatter);
-			
-			$('#page-content').html(htmlContent);
+					htmlContent = converter.makeHtml(markdownWithoutFrontMatter);
+					
+					$('#page-content').html(htmlContent);
+			}
 		});
 	}
 	
